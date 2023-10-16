acef45efd203f47496c9b417e7c5e49a86270038
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-10-16 18:15:33 +0700

    fix ui bug2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -35,50 +35,60 @@
                             FKIP UNIB</span>
                     </a>
                 </div>
-                <div class="flex items-center">
-                    <div class="flex items-center ml-3">
-                        <div>
+                <div class="flex justify-center items-center">
+                    <x-bell />
+                    <div class="flex items-center">
+                        <div class="flex items-center ml-3">
+
                             <button type="button"
                                 class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                                 aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                 <span class="sr-only">Open user menu</span>
+                                @php
+                                    $avatar = 'https://ui-avatars.com/api/?name=' . $authUser->name . '&background=random';
+                                @endphp
                                 <img class="w-8 h-8 rounded-full"
-                                    src="{{ $authUser->profile_image ? asset('/storage' . $authUser->profile_image) : asset('images/default-profile.webp') }}"
+                                    src="{{ $authUser->profile_image ? asset('/storage' . $authUser->profile_image) : $avatar }}"
                                     alt="user photo">
                             </button>
-                        </div>
-                        <div class="z-30 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
-                            id="dropdown-user">
-                            <div class="px-4 py-3" role="none">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                    {{ $authUser->name }}
-                                </p>
-                                <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                    {{ $authUser->username }}
-                                </p>
 
+                            <div class="z-30 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                                id="dropdown-user">
+                                <div class="px-4 py-3" role="none">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+                                        role="none">
+                                        {{ $authUser->name }}
+                                    </p>
+                                    <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                        {{ $authUser->username }}
+                                    </p>
+
+                                </div>
+                                <ul class="py-1" role="none">
+                                    <li>
+                                        <a href="#"
+                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                            role="menuitem">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                            role="menuitem">Settings</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                            role="menuitem">Sign out</a>
+                                    </li>
+                                </ul>
                             </div>
-                            <ul class="py-1" role="none">
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Dashboard</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Settings</a>
-                                </li>
-
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Sign out</a>
-                                </li>
-                            </ul>
                         </div>
                     </div>
+                    <p class="hidden sm:block ml-1 text-slate-600 font-semibold">{{ $authUser->name }}</p>
+
                 </div>
+
             </div>
         </div>
     </nav>
--- resources/views/components/sidebar-list.blade.php
+++ resources/views/components/sidebar-list.blade.php
@@ -3,7 +3,7 @@
 @if (!$list['dropdown'])
     <li>
         <a href="/{{ $list['link'] }}"
-            class="{{ request()->is($list['link']) ? 'bg-slate-300' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            class="{{ request()->is($list['link']) ? 'bg-slate-200' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
             <img class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                 src="{{ $list['icon'] }}" alt="">
             <span class="ml-3">{{ $list['title'] }}</span>
@@ -27,7 +27,7 @@
             @foreach ($list['dropdown'] as $sublist)
                 <li>
                     <a href="{{ $sublist['link'] }}"
-                        class="{{ Str::contains(request()->path(), $sublist['link']) ? 'bg-slate-300' : '' }} flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ $sublist['title'] }}</a>
+                        class="{{ request()->url() == $sublist['link'] ? 'bg-slate-200' : '' }} flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ $sublist['title'] }}</a>
 
                 </li>
             @endforeach
