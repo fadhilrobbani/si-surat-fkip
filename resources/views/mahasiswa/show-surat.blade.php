83be9d41354c8b992e2fc460ee51cad358cf70f8
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-11-13 16:01:47 +0700

    add stepper

--- resources/views/mahasiswa/show-surat.blade.php
+++ resources/views/mahasiswa/show-surat.blade.php
@@ -96,35 +96,102 @@
                     </tr>
                 @endforeach
             @endif
-            </table>
+        </table>
 
-            <div>
-                <ol
-                    class="relative mx-8 text-gray-500 border-l border-gray-200 dark:border-gray-700 dark:text-gray-400">
-                    <li class="mb-10 ml-6">
-                        @if (count($step) == 0)
-                            <span
-                                class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
-                                <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                                    <path
-                                        d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
-                                </svg>
-                            </span>
-                        @else
-                            <span
-                                class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
-                                <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                                </svg>
-                            </span>
-                        @endif
+        <div>
+            <ol class="relative mx-8 text-gray-500 border-l border-gray-200 dark:border-gray-700 dark:text-gray-400">
+                <li class="mb-10 ml-6">
+                    <span
+                        class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
+                        <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                            <path
+                                d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                        </svg>
+                    </span>
+                    <span
+                        class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                        <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5.917 5.724 10.5 15 1.5" />
+                        </svg>
+                    </span>
+                    <h3 class="font-medium leading-tight">Mahasiswa</h3>
+                    <p class="text-sm">Berhasil Mengajukan</p>
+                </li>
+                <li class="mb-10 ml-6">
+
+                    @if (
+                        $surat->current_user->role->id == 4 ||
+                            $surat->current_user->role->id == 5 ||
+                            $surat->current_user->role->id == 6 ||
+                            $surat->current_user->role->id == 2)
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                            <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                        </span>
                         <h3 class="font-medium leading-tight">Staff</h3>
-                        <p class="text-sm">Step details here</p>
-                    </li>
-                    <li class="mb-10 ml-6">
+                        <p class="text-sm">Disetujui</p>
+                    @elseif ($surat->status == 'denied')
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-pink-300 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                            {{-- <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg> --}}
+
+                            <svg class="w-3.5 h-3.5 text-rose-700 dark:text-rose-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                              </svg>
+
+                        </span>
+                        <h3 class="font-medium leading-tight">Staff</h3>
+                        <p class="text-sm">{{ $surat->current_user->role->id == 3 ? 'Ditolak' : 'Gagal Diteruskan' }}</p>
+                    @else
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
+                            <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                <path
+                                    d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                            </svg>
+                        </span>
+                        <h3 class="font-medium leading-tight">Staff</h3>
+                        <p class="text-sm">Menunggu</p>
+                    @endif
+
+                </li>
+                <li class="mb-10 ml-6">
+
+                    @if ($surat->current_user->role->id == 5 || $surat->current_user->role->id == 6 || $surat->current_user->role->id == 2)
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                            <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                        </span>
+                        <h3 class="font-medium leading-tight">Kaprodi</h3>
+                        <p class="text-sm">Disetujui</p>
+                    @elseif ($surat->status == 'denied')
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-pink-300 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+
+                            <svg class="w-3.5 h-3.5 text-rose-700 dark:text-rose-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                              </svg>
+
+                        </span>
+                        <h3 class="font-medium leading-tight">Kaprodi</h3>
+                        <p class="text-sm">{{ $surat->current_user->role->id == 4 ? 'Ditolak' : 'Gagal Diteruskan' }}</p>
+                    @else
                         <span
                             class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
                             <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
@@ -134,9 +201,35 @@
                             </svg>
                         </span>
                         <h3 class="font-medium leading-tight">Kaprodi</h3>
-                        <p class="text-sm">Step details here</p>
-                    </li>
-                    <li class="mb-10 ml-6">
+                        <p class="text-sm">Menunggu</p>
+                    @endif
+
+                </li>
+                <li class="mb-10 ml-6">
+
+                    @if ($surat->current_user->role->id == 6 || $surat->current_user->role->id == 2)
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                            <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                        </span>
+                        <h3 class="font-medium leading-tight">Wakil Dekan 1</h3>
+                        <p class="text-sm">Disetujui</p>
+                        @elseif ($surat->status == 'denied')
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-pink-300 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+
+                            <svg class="w-3.5 h-3.5 text-rose-700 dark:text-rose-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                              </svg>
+
+                        </span>
+                        <h3 class="font-medium leading-tight">Wakil Dekan 1</h3>
+                        <p class="text-sm">{{ $surat->current_user->role->id == 5 ? 'Ditolak' : 'Gagal Diteruskan' }}</p>
+                    @else
                         <span
                             class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
                             <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
@@ -146,9 +239,35 @@
                             </svg>
                         </span>
                         <h3 class="font-medium leading-tight">Wakil Dekan 1</h3>
-                        <p class="text-sm">Step details here</p>
-                    </li>
-                    <li class="ml-6">
+                        <p class="text-sm">Menunggu</p>
+                    @endif
+
+                </li>
+                <li class="ml-6">
+
+                    @if ($surat->current_user->role->id == 2)
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                            <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                            </svg>
+                        </span>
+                        <h3 class="font-medium leading-tight">Akademik</h3>
+                        <p class="text-sm">Disetujui</p>
+                        @elseif ($surat->status == 'denied')
+                        <span
+                            class="absolute flex items-center justify-center w-8 h-8 bg-pink-300 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+
+                            <svg class="w-3.5 h-3.5 text-rose-700 dark:text-rose-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                              </svg>
+
+                        </span>
+                        <h3 class="font-medium leading-tight">Akademik</h3>
+                        <p class="text-sm">{{  $surat->current_user->role->id  == 6 ? 'Ditolak' : 'Gagal Diteruskan' }}</p>
+                    @else
                         <span
                             class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
                             <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
@@ -158,25 +277,27 @@
                             </svg>
                         </span>
                         <h3 class="font-medium leading-tight">Akademik</h3>
-                        <p class="text-sm">Step details here</p>
-                    </li>
-                </ol>
-            </div>
-        </div>
+                        <p class="text-sm">Menunggu</p>
+                    @endif
 
-        {{-- <embed
+                </li>
+            </ol>
+        </div>
+    </div>
+
+    {{-- <embed
     src="{{ route('show-file',basename($surat->files['ijazah'])) }}"
     style="width:600px; height:800px;"
     frameborder="0"> --}}
 
-        @if ($surat->status == 'finished')
-            <a href="{{ route('print-surat-mahasiswa', $surat->id) }}"> <button type="button"
-                    class="text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Cetak</button></a>
-        @else
-            <button type="button" disabled
-                class="text-white cursor-not-allowed mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Cetak</button>
-            <p class="italic text-slate-500">Surat belum dapat dicetak selama surat belum disetujui</p>
-        @endif
+    @if ($surat->status == 'finished')
+        <a href="{{ route('print-surat-mahasiswa', $surat->id) }}"> <button type="button"
+                class="text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Cetak</button></a>
+    @else
+        <button type="button" disabled
+            class="text-white cursor-not-allowed mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Cetak</button>
+        <p class="italic text-slate-500">Surat belum dapat dicetak selama surat belum disetujui</p>
+    @endif
 
 
 </x-layout>
