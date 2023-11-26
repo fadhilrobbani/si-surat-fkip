5d580feb401ca43a6829d6b6a5da22f3030e1c15
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-11-26 14:05:42 +0700

    add surat permohonan izin penelitian

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -45,6 +45,13 @@ class PDFController extends Controller
                 'chroot' => public_path()
             ]);
         }
+
+        if ($surat->jenisSurat->slug == 'surat-permohonan-izin-penelitian-mahasiswa') {
+            $pdf = Pdf::loadview('template.surat-permohonan-izin-penelitian-mahasiswa', ['surat' => $surat])->setPaper('a4', 'potrait')->setOptions([
+                'tempDir' => public_path(),
+                'chroot' => public_path()
+            ]);
+        }
         return $pdf->stream();
     }
 
---
+++ resources/views/mahasiswa/formsurat/form-keterangan-izin-penelitian.blade.php
@@ -0,0 +1,138 @@
+@php
+    $authUser = auth()->user();
+@endphp
+<x-layout :authUser='$authUser'>
+    <x-slot:title>
+        Mahasiswa | Pengajuan Surat
+    </x-slot:title>
+    {{-- <x-breadcumb /> --}}
+    {{ Breadcrumbs::render('pengajuan-surat-form', $jenisSurat) }}
+    <p class="font-bold text-lg mx-auto text-center mb-2">Surat Permohonan Izin Penelitian</p>
+    <form action="{{ route('store-surat', $jenisSurat->slug) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('post')
+        <div class="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama<span
+                        class="text-red-500">*</span></label>
+                <input type="text" id="disabled-input-2" aria-label="disabled input 2" readonly name="name"
+                    class="bg-gray-50 cursor-not-allowed border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Masukkan Nama Lengkap" value="{{ $authUser->name }}" required>
+            </div>
+            <div>
+                <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NPM<span
+                        class="text-red-500">*</span></label>
+                <input type="text" id="username" name="username"
+                    class=" cursor-not-allowed bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Masukkan NPM Anda" value="{{ $authUser->username }}" readonly required>
+            </div>
+            <div>
+                <label for="program-studi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih
+                    Program Studi<span class="text-red-500">*</span></label>
+                <select id="program-studi" disabled
+                    class="bg-gray-50 cursor-not-allowed border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    @foreach ($daftarProgramStudi as $programStudi)
+                        <option {{ $authUser->program_studi_id == $programStudi->id ? 'selected' : '' }}
+                            value="{{ $programStudi->id }}">{{ $programStudi->name }}</option>
+                    @endforeach
+                </select>
+                <input class="hidden" name="program-studi" type="text" value="{{ $authUser->programStudi->id }}">
+            </div>
+
+            <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email<span
+                        class="text-red-500">*</span></label>
+                <input type="email" id="email" name="email" readonly
+                    class="bg-gray-50 border cursor-not-allowed border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Masukkan email yang aktif" value="{{ $authUser->email }}" required>
+            </div>
+            <div>
+                <label for="tujuan1" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tujuan
+                    Surat<span class="text-red-500">*</span></label>
+                <input type="text" name="tujuan1"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Contoh: Kepala SMA Negeri 365 Kota Bengkulu" value="{{ old('tujuan1') }}" required>
+            </div>
+            <div>
+                <label for="tujuan2" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tujuan Surat
+                    Ke-2 (Opsional)</label>
+                <input type="text" name="tujuan2"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Contoh: Kepala Dinas Pendidikan dan Kebudayaan Kota Bengkulu"
+                    value="{{ old('tujuan2') }}">
+            </div>
+            <div>
+                <label for="tujuan3" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tujuan
+                    Surat Ke-3 (Opsional)</label>
+                <input type="text" name="tujuan3"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Contoh: Kepala Badan Kesatuan Bangsa dan Politik Kota Bengkulu"
+                    value="{{ old('tujuan3') }}">
+            </div>
+            <div>
+                <label for="judul-skripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Judul
+                    Skripsi<span class="text-red-500">*</span></label>
+                <input type="text" name="judul-skripsi"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Judul Skripsi Anda" value="{{ old('judul-skripsi') }}" required>
+            </div>
+            <div>
+                <label for="tempat-penelitian"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tempat Penelitian<span
+                        class="text-red-500">*</span></label>
+                <input type="text" name="tempat-penelitian"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Contoh:  SMA Negeri 365 Kota Bengkulu" value="{{ old('tempat-penelitian') }}"
+                    required>
+            </div>
+            <div>
+                <label for="waktu-mulai-penelitian"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waktu Mulai Penelitian<span
+                        class="text-red-500">*</span></label>
+                <input type="date" id="waktu-mulai-penelitian" name="waktu-mulai-penelitian"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Pilih waktu kapan Anda akan memulai penelitian"
+                    value="{{ old('waktu-mulai-penelitian') }}" required>
+            </div>
+            <div>
+                <label for="waktu-selesai-penelitian"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waktu Selesai Penelitian<span
+                        class="text-red-500">*</span></label>
+                <input type="date" id="waktu-selesai-penelitian" name="waktu-selesai-penelitian"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Pilih waktu kapan Anda akan selesai dalam penelitian"
+                    value="{{ old('waktu-selesai-penelitian') }}" required>
+            </div>
+
+
+            <div>
+
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    for="berkas-proposal">Upload
+                    Lampiran Berkas Proposal<span class="text-red-500">*</span></label>
+                <input
+                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    aria-describedby="file_input_help" id="berkas-proposal" type="file" name="berkas-proposal"
+                    accept=".pdf" max-size="2048" required>
+                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">Format PDF
+                    (MAX.
+                    2 MB).</p>
+
+            </div>
+
+
+        </div>
+
+        <x-modal-send :daftarPenerima='$daftarPenerima' />
+        <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
+            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button">
+            Ajukan Surat
+        </button>
+        {{-- <button type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button> --}}
+    </form>
+
+
+
+</x-layout>
