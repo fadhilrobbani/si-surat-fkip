f85b8ed6c8ed56a932cb37c47007adf003e773e1
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-12-18 19:32:17 +0700

    update profil navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -56,16 +56,16 @@
 
                             <div class="z-30 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
                                 id="dropdown-user">
-                                <div class="px-4 py-3" role="none">
+                                {{-- <div class="px-4 py-3" role="none">
                                     <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
                                         role="none">
                                         {{ $authUser->name }}
                                     </p>
-                                    <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                    <p class="text-sm text-gray-900  dark:text-white" role="none">
                                         {{ $authUser->username }}
                                     </p>
 
-                                </div>
+                                </div> --}}
                                 <ul class="py-1" role="none">
                                     <li>
                                         <a href="/"
