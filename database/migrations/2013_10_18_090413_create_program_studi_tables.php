678dae68dc3bb5bfb02129521085cb8535cacbe1
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-12-18 22:10:48 +0700

    fix migrate and seeder prodi

--- database/migrations/2013_10_18_090413_create_program_studi_tables.php
+++ database/migrations/2013_10_18_090413_create_program_studi_tables.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('jurusan_id');
-            $table->char('kode', 3);
+            $table->string('kode', 4);
             $table->foreign('jurusan_id')->references('id')->on('jurusan_tables')->cascadeOnDelete();
             $table->timestamps();
         });
