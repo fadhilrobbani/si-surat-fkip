4afe824f47e7e6bf8c6c01cd93f8a9c2b7a0ec17
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-12-19 21:47:41 +0700

    update filament

--- resources/views/filament/resources/jurusan-resource/widgets/warning-action.blade.php
+++ resources/views/filament/resources/jurusan-resource/widgets/warning-action.blade.php
@@ -1,7 +1,7 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div class="flex flex-col gap-4">
-            <h1 class="text-pink-600 text-center font-semibold text-xl">Peringatan!</h1>
+            <h1 class="text-red-600 text-center font-semibold text-xl">PERINGATAN!</h1>
             <p class="text-center">Menghapus data jurusan yang ada akan menghapus semua akun dan program studi yang terkait dengan jurusan yang dihapus!</p>
         </div>
     </x-filament::section>
--- resources/views/filament/resources/program-studi-resource/widgets/warning-action.blade.php
+++ resources/views/filament/resources/program-studi-resource/widgets/warning-action.blade.php
@@ -2,7 +2,7 @@
     <x-filament::section>
         <div class="flex flex-col gap-4">
 
-            <h1 class="text-pink-600 text-center font-semibold text-xl">Peringatan!</h1>
+            <h1 class="text-red-600 text-center font-semibold text-xl">PERINGATAN!</h1>
             <p class="text-center">Menghapus data program studi yang ada akan menghapus semua akun yang terkait dengan program studi yang dihapus!</p>
         </div>
     </x-filament::section>
