b55afb222830d320894b8d3127178ebb3b64a42b
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-01-02 14:40:25 +0700

    fix bug preview staff surat prapenelitian

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -53,6 +53,12 @@ class PDFController extends Controller
                 'chroot' => public_path()
             ]);
         }
+        if ($surat->jenisSurat->slug == 'surat-permohonan-izin-prapenelitian-mahasiswa') {
+            $pdf = Pdf::loadview('template.surat-permohonan-izin-prapenelitian-mahasiswa', ['surat' => $surat])->setPaper('a4', 'potrait')->setOptions([
+                'tempDir' => public_path(),
+                'chroot' => public_path()
+            ]);
+        }
 
         if ($surat->jenisSurat->slug == 'surat-keterangan-kesalahan-ijazah') {
             $pdf = Pdf::loadview('template.surat-keterangan-kesalahan-ijazah', ['surat' => $surat])->setPaper('a4', 'potrait')->setOptions([
