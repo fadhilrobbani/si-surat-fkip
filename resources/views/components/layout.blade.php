2c7b3152adb85ea045302f3a80a1bfd3b3d8a017
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-11-03 01:25:21 +0700

    update ui blue

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -549,7 +549,7 @@
 
     <div class="p-4 sm:ml-64">
         <div
-            class="p-4 border-2 relative overflow-x-hidden  border-gray-200 border-dashed rounded-lg  dark:border-gray-700 mt-14">
+            class="p-4 relative overflow-x-hidden  border-gray-200 border-dashed rounded-lg  dark:border-gray-700 mt-14">
             {{ $slot }}
         </div>
     </div>
