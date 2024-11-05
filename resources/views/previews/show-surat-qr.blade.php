0c1cebfc64a76a90218a45d7ca5e90c4b9a92ecb
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-11-05 13:39:08 +0700

    detail show qr surat

--- resources/views/previews/show-surat-qr.blade.php
+++ resources/views/previews/show-surat-qr.blade.php
@@ -16,48 +16,165 @@
         <img class="w-28 mx-auto mb-5" src="{{ asset('images/logounib.png') }}" alt="logounib" />
 
         <h1 class="text-2xl font-bold text-center text-blue-800">Halaman Validasi Keaslian Surat</h1>
-        <p class="text-center text-gray-700 mt-2 mb-6">Dengan ini menyatakan bahwa surat ini adalah asli atau resmi
-            diterbitkan dari FKIP UNIB</p>
+        <p class="text-center text-gray-700 mt-2 mb-6">Dengan ini menyatakan bahwa surat ini adalah benar
+            diterbitkan dari FKIP UNIB. </p>
 
-        <div class="overflow-x-auto shadow-lg rounded-lg">
-            <table class="w-full text-sm text-left text-gray-700 bg-white">
-                <tbody>
-                    <tr class="border-b">
-                        <td class="px-4 py-3 font-semibold bg-gray-50">Nama:</td>
-                        <td class="px-4 py-3">{{ $surat->data['nama'] }}</td>
-                    </tr>
-                    <tr class="border-b">
-                        <td class="px-4 py-3 font-semibold bg-gray-50">Username:</td>
-                        <td class="px-4 py-3">
-                            {{ isset($surat->data['npm']) ? $surat->data['npm'] : $surat->data['username'] }}</td>
-                    </tr>
-                    <tr class="border-b">
-                        <td class="px-4 py-3 font-semibold bg-gray-50">Jenis Surat:</td>
-                        <td class="px-4 py-3">{{ $surat->jenisSurat->name }}</td>
-                    </tr>
-                    <tr class="border-b">
-                        <td class="px-4 py-3 font-semibold bg-gray-50">Nomor Surat:</td>
-                        <td class="px-4 py-3">{{ $surat->data['noSurat'] }}</td>
-                    </tr>
-                    <tr class="border-b">
-                        <td class="px-4 py-3 font-semibold bg-gray-50">Yang menandatangani:</td>
-                        <td class="px-4 py-3">
-                            {{ $surat->data['private']['namaWD1'] ?? ($surat->data['private']['namaWD'] ?? ($surat->data['private']['namaDekan'] ?? '(Nama tidak tersedia)')) }}
-                        </td>
-                    </tr>
-                    <tr class="border-b">
-                        <td class="px-4 py-3 font-semibold bg-gray-50">Tanggal Surat Diterbitkan:</td>
-                        <td class="px-4 py-3">{{ $surat->data['tanggal_selesai'] }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        @if ($surat->jenisSurat->user_type == 'mahasiswa')
+            <div class="overflow-x-auto shadow-lg rounded-lg">
+                <table class="w-full text-sm text-left text-gray-700 bg-white">
+                    <tbody>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Nomor Surat:</td>
+                            <td class="px-4 py-3">{{ $surat->data['noSurat'] }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Tanggal Surat Diterbitkan:</td>
+                            <td class="px-4 py-3">{{ $surat->data['tanggal_selesai'] }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Nama:</td>
+                            <td class="px-4 py-3">{{ $surat->data['nama'] }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">NPM:</td>
+                            <td class="px-4 py-3">
+                                {{ isset($surat->data['npm']) ? $surat->data['npm'] : $surat->data['username'] }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Jenis Surat:</td>
+                            <td class="px-4 py-3">{{ $surat->jenisSurat->name }}</td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Ditandatangani oleh:</td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['namaWD1'] ?? ($surat->data['private']['namaWD'] ?? ($surat->data['private']['namaDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50"></td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['deksripsiWD1'] ?? ($surat->data['private']['deskripsiWD'] ?? ($surat->data['private']['deskripsiDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50"></td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['nipWD1'] ?? ($surat->data['private']['nipWD'] ?? ($surat->data['private']['nipDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
+        @if (
+            ($surat->jenisSurat->user_type == 'staff' && $surat->jenisSurat->slug == 'surat-tugas') ||
+                ($surat->jenisSurat->user_type == 'staff' && $surat->jenisSurat->slug == 'surat-tugas-kelompok'))
+            <div class="overflow-x-auto shadow-lg rounded-lg">
+                <table class="w-full text-sm text-left text-gray-700 bg-white">
+                    <tbody>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Nomor Surat:</td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['noSurat'] . '/UN30.7/KP/' . $surat->created_at->year }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Tanggal Surat Diterbitkan:</td>
+                            <td class="px-4 py-3">{{ $surat->data['tanggal_selesai'] }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Jenis Surat / Perihal:</td>
+                            <td class="px-4 py-3">{{ $surat->jenisSurat->name }}</td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Ditandatangani oleh:</td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['namaWD1'] ?? ($surat->data['private']['namaWD'] ?? ($surat->data['private']['namaDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50"></td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['deksripsiWD1'] ?? ($surat->data['private']['deskripsiWD'] ?? ($surat->data['private']['deskripsiDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50"></td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['nipWD1'] ?? ($surat->data['private']['nipWD'] ?? ($surat->data['private']['nipDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
+        @if ($surat->jenisSurat->user_type == 'staff-dekan' && $surat->jenisSurat->slug == 'surat-keluar')
+            <div class="overflow-x-auto shadow-lg rounded-lg">
+                <table class="w-full text-sm text-left text-gray-700 bg-white">
+                    <tbody>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Nomor Surat:</td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['noSurat'] . '/UN30.7/PP/' . $surat->created_at->year }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Tanggal Surat Diterbitkan:</td>
+                            <td class="px-4 py-3">{{ $surat->data['tanggal_selesai'] }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Jenis Surat / Perihal:</td>
+                            <td class="px-4 py-3">{{ $surat->data['perihal'] }}</td>
+                        </tr>
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50">Ditandatangani oleh:</td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['namaWD1'] ?? ($surat->data['private']['namaWD'] ?? ($surat->data['private']['namaDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50"></td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['deksripsiWD1'] ?? ($surat->data['private']['deskripsiWD'] ?? ($surat->data['private']['deskripsiDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                        <tr class="border-b">
+                            <td class="px-4 py-3 font-semibold bg-gray-50"></td>
+                            <td class="px-4 py-3">
+                                {{ $surat->data['private']['nipWD1'] ?? ($surat->data['private']['nipWD'] ?? ($surat->data['private']['nipDekan'] ?? '(Nama tidak tersedia)')) }}
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
 
         @php
             $url = URL::signedRoute('cetak-surat-qr', ['surat' => $surat->id]);
         @endphp
+        <p class="text-center text-sm text-gray-700 mt-4 mb-6">Untuk memastikan bahwa Anda mengakses data surat yang
+            benar, pastikan URL pada
+            browser berasal dari <a class="underline" href=" https://esurat-fkip.unib.ac.id">
+                https://esurat-fkip.unib.ac.id</a></p>
+
+        {{-- <a href="{{ route('lihat-html-surat-qr', ['surat' => $surat->id]) }}">html</a> --}}
+        <iframe src="{{ $url }}" width="100%" height="600"></iframe>
+
         <a href="{{ $url }}"
-            class="block text-center mt-8 px-6 py-3 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-700">Cetak</a>
+            class="block text-center mt-8 px-6 py-3 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-700">Unduh</a>
     </main>
 </body>
 
