f579a97d57d916686035dc8e33d78798b11350a4
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-11-08 00:44:23 +0700

    fix can't add new tembusan when edit surat keluar

--- resources/views/staff-dekan/formsurat/edit-form-surat-keluar.blade.php
+++ resources/views/staff-dekan/formsurat/edit-form-surat-keluar.blade.php
@@ -233,7 +233,7 @@
 
         <p class="font-semibold text-slate-500 text-md mx-auto mt-6 mb-2">Tembusan (Opsional):</p>
 
-        <div x-data="{ tembusan: {{ json_encode($surat->data['private']['tembusan']) }} }" class="bg-slate-100 p-4 mb-6 rounded-lg">
+        <div x-data="{ tembusan: {{ json_encode($surat->data['private']['tembusan'] ?? []) }} }" class="bg-slate-100 p-4 mb-6 rounded-lg">
             <!-- Section for tembusan input fields -->
             <template x-for="(item, index) in tembusan" :key="index">
                 <div class="flex gap-4 mb-4">
@@ -254,7 +254,7 @@
             </template>
             <!-- Button to add new tembusan field -->
             <button type="button" @click="tembusan.push('')"
-                class="  text-white bg-slate-500 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-80">
+                class="text-white bg-slate-500 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-80">
                 <span class="flex flex-row items-center justify-center gap-2">
                     <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
@@ -266,6 +266,7 @@
                 </span>
             </button>
         </div>
+
 
         <div class="flex justify-center gap-4">
             <button type="submit"
