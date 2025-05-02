84efa0b7c853ae34f5d8aae985468fd5ff008344
fadhilrobbani <rabbanifadhillailham@gmail.com>
2025-05-02 15:21:02 +0700

    Feat: Disabled shippin option, only used "dikirim" as default

--- resources/views/mahasiswa/formlegalisir/form-legalisir-transkrip.blade.php
+++ resources/views/mahasiswa/formlegalisir/form-legalisir-transkrip.blade.php
@@ -290,9 +290,9 @@
             Pengiriman<span class="text-red-500">*</span></label>
         <select id="pengiriman" name="pengiriman"
             class="bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            onchange="toggleShippingInputs()">
+            onchange="toggleShippingInputs()" disabled>
             <option value="dikirim">Dikirim via COD (J&T)</option>
-            <option value="ambil">Ambil di Tempat (Akademik FKIP UNIB)</option>
+            {{-- <option value="ambil">Ambil di Tempat (Akademik FKIP UNIB)</option> --}}
         </select>
         <div id="shippingInputs" class="bg-slate-50 p-4 rounded-lg shadow-lg">
             <div class="grid gap-6 md:grid-cols-1">
